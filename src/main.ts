8aed81f8f9d01d872eaed0940b87ba6fb23a7089
Eduardo Urbina <eduardo.urbina@lineup.com.mx>
2024-02-13 22:31:40 -0600

    added cors policy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors()); // Enable CORS with default settings
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
